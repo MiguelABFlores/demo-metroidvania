bbd8b921de6c58707fc486930e919006208b7fba
Miguel Angel Briseno Flores <miguel.briseno@oracle.com>
2025-07-03 00:43:31 -0600

    Initial commit on development branch

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,176 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Transform groundPoint;
+    private bool isOnGround;
+    public LayerMask whatIsGround;
+
+    public Animator anim;
+
+    public BulletController shotToFire;
+    public Transform shotPoint;
+
+    private bool canDoubleJump;
+
+    public float dashSpeed, dashTime;
+    private float dashCounter;
+
+    public SpriteRenderer theSR, afterImage;
+    public float afterImageLifetime, timeBetweenAfterImages;
+    private float afterImageCounter;
+    public Color afterImageColor;
+
+    public float waitAfterDashing;
+    private float dashRechargeCounter;
+
+    public GameObject standing, ball;
+    public float waitToBall;
+    private float ballCounter;
+    public Animator ballAnim;
+
+    public Transform bombPoint;
+    public GameObject bomb;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (dashRechargeCounter > 0)
+        {
+            dashRechargeCounter -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire2") && standing.activeSelf)
+            {
+                dashCounter = dashTime;
+                ShowAfterImage();
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter = dashCounter - Time.deltaTime;
+            theRB.velocity = new Vector2(dashSpeed * transform.localScale.x, theRB.velocity.y);
+            afterImageCounter -= Time.deltaTime;
+            if (afterImageCounter <= 0)
+            {
+                ShowAfterImage();
+            }
+            dashRechargeCounter = waitAfterDashing;
+        }
+        else
+        {
+            // Character Movement
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+
+            // Handle Character Movement Direction
+            if (theRB.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (theRB.velocity.x > 0)
+            {
+                transform.localScale = Vector3.one;
+            }
+        }
+        // Character on Ground
+        isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
+
+        // Character Jumping Condition
+        if (Input.GetButtonDown("Jump") && (isOnGround || canDoubleJump))
+        {
+            if (isOnGround)
+            {
+                canDoubleJump = true;
+            }
+            else
+            {
+                canDoubleJump = false;
+                anim.SetTrigger("doubleJump");
+            }
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+        // Shooting
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (standing.activeSelf)
+            {
+                Instantiate(shotToFire, shotPoint.position, shotPoint.rotation).moveDir = new Vector2(transform.localScale.x, 0f);
+                anim.SetTrigger("shotFired");
+            }
+            else if (ball.activeSelf)
+            {
+                Instantiate(bomb, bombPoint.position, bombPoint.rotation);
+            }
+        }
+
+        // Ball Mode
+        if (!ball.activeSelf)
+        {
+            if (Input.GetAxisRaw("Vertical") < -0.9f)
+            {
+                ballCounter -= Time.deltaTime;
+                if (ballCounter <= 0)
+                {
+                    ball.SetActive(true);
+                    standing.SetActive(false);
+                }
+            }
+            else
+            {
+                ballCounter = waitToBall;
+            }
+        }
+        else
+        {
+            if (Input.GetAxisRaw("Vertical") > 0.9f)
+            {
+                ballCounter -= Time.deltaTime;
+                if (ballCounter <= 0)
+                {
+                    ball.SetActive(false);
+                    standing.SetActive(true);
+                }
+            }
+            else
+            {
+                ballCounter = waitToBall;
+            }
+        }
+
+        // Character Animation
+        if (standing.activeSelf)
+        {
+            anim.SetBool("isOnGround", isOnGround);
+            anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+        }
+        if (ball.activeSelf)
+        {
+            ballAnim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+        }
+    }
+
+    public void ShowAfterImage()
+    {
+        SpriteRenderer image = Instantiate(afterImage, transform.position, transform.rotation);
+        image.sprite = theSR.sprite;
+        image.transform.localScale = transform.localScale;
+        image.color = afterImageColor;
+        Destroy(image.gameObject, afterImageLifetime);
+        afterImageCounter = timeBetweenAfterImages;
+    }
+}
